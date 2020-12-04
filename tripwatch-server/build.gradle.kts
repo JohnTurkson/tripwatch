c0b50cc85873186074f717c69223a72aafcd346b
John Turkson <git@johnturkson.com>
2020-12-04 02:06:25 -0800

    Add aws-tools dependencies

--- tripwatch-server/build.gradle.kts
+++ tripwatch-server/build.gradle.kts
@@ -7,6 +7,15 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.20")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-object-builder:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-request-builder:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-transforming-serializer:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-dynamodb-request-handler:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-request-signer:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-request-handler:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-ses-request-builder:0.0.37")
+    implementation("com.johnturkson.aws-tools:aws-ses-request-handler:0.0.37")
+    implementation(project(":tripwatch-common"))
 }
 
 tasks.register<Copy>("copyLambdaLayerDependencies") {
