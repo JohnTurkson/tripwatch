c88cb1224975e0bfe45d9157e94b97a69ba1537a
Andy Ma <solarcactus@live.com>
2020-12-04 15:48:50 -0800

    Added colors, font and themes

--- tripwatch-android/app/src/main/java/com/johnturkson/tripwatch/android/ui/TripwatchApp.kt
+++ tripwatch-android/app/src/main/java/com/johnturkson/tripwatch/android/ui/TripwatchApp.kt
@@ -2,13 +2,16 @@ package com.johnturkson.tripwatch.android.ui
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.runtime.Composable
+import com.johnturkson.tripwatch.android.ui.themes.TripwatchTheme
 
 @Composable
 fun TripwatchApp(navigationViewModel : NavigationViewModel) {
-    Crossfade(navigationViewModel.currentScreen) { screen ->
-        when(screen) {
-            is Screen.Launch -> LaunchScreen(navigationViewModel)
-            is Screen.Home -> HomeScreen(navigationViewModel)
+    TripwatchTheme {
+        Crossfade(navigationViewModel.currentScreen) { screen ->
+            when (screen) {
+                is Screen.Launch -> LaunchScreen(navigationViewModel)
+                is Screen.Home -> HomeScreen(navigationViewModel)
+            }
         }
     }
 }
