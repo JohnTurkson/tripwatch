ddaf7f217a366ecf19407ed71881edcc1f279030
John Turkson <git@johnturkson.com>
2020-12-04 02:05:51 -0800

    Add gradle task to build Lambda layer

--- tripwatch-server/build.gradle.kts
+++ tripwatch-server/build.gradle.kts
@@ -8,3 +8,15 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.1")
 }
+
+tasks.register<Copy>("copyLambdaLayerDependencies") {
+    from(configurations.runtimeClasspath)
+    into("$buildDir/lambda/layer/java/lib")
+}
+
+tasks.register<Zip>("buildLambdaLayer") {
+    archiveFileName.set("tripwatch-kotlin-lambda-layer.zip")
+    destinationDirectory.set(file("$buildDir/lambda"))
+    from("$buildDir/lambda/layer")
+    dependsOn("copyLambdaLayerDependencies")
+}
