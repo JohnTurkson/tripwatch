f765e04c64a4f0c9e0934b77aad2a2e7a221eb48
Andy Ma <solarcactus@live.com>
2020-12-05 00:31:06 -0800

    Added android code to get builds working again

--- tripwatch-android/app/src/main/java/com/johnturkson/tripwatch/android/utils/LoginUtils.kt
+++ tripwatch-android/app/src/main/java/com/johnturkson/tripwatch/android/utils/LoginUtils.kt
@@ -2,13 +2,8 @@ package com.johnturkson.tripwatch.android.utils
 
 import com.johnturkson.tripwatch.common.data.UserData
 import com.johnturkson.tripwatch.common.requests.Request
-import com.johnturkson.tripwatch.common.requests.Request.CreateUserRequest
 import com.johnturkson.tripwatch.common.responses.Response
 import io.ktor.client.*
-import io.ktor.client.engine.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.serialization.json.Json
@@ -17,7 +12,7 @@ suspend fun requestLogIn(userData : UserData) : Response {
     val client = HttpClient()
 
     val response = client.post<HttpStatement>("https://tripwatch.johnturkson.com/api/CreateUser") {
-        body = Json.encodeToString(Request.serializer(), CreateUserRequest(userData))
+        body = Json.encodeToString(Request.serializer(), Request.CreateUserRequest(userData))
         println(body)
     }
 
